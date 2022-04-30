5b94c0c30650021f2a2340a8c4311005ae3a2a12
Janis Vilks <janis@shipppingtechnology.com>
2022-04-30 11:44:48 +0200

    pass articles

--- app_extension/inject.js
+++ app_extension/inject.js
@@ -1,23 +1,25 @@
 const __extension_carbon_footprint_assistant_init__ = async () => {
-    console.log("gggggg");
+    console.log("Inject script initialized");
 }
 
 __extension_carbon_footprint_assistant_init__()
-    .then(() => retreive_articles())
-    .then(() => send_articles())
+    .then(() => retreive_articles_from_html())
+    .then((articles) => send_articles(articles))
     .catch((e) => {
         console.log(e)
     });
 
 
-const retreive_articles = async () => {
+const retreive_articles_from_html = async () => {
     let articles = document.getElementsByTagName("article");
 
     for (var i = 0; i < articles.length; i++) {
         articles[i].style.background = "red";
     }
+    
+    return articles
 };
 
-const send_articles = async () => {
+const send_articles = async (articles) => {
     console.log("ARTICLES");
 };
